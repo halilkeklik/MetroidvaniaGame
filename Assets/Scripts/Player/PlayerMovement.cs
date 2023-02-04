dd0f554ca1c4b16f39270e533d22c40f908328ad
halilkeklik <halil@halilkeklik.com>
2023-02-04 17:41:04 +0300

    Added player run animation.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,23 +5,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
     [SerializeField] private float speed;
+    [SerializeField] private float jumpforce;
+    private float dirX;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
         
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 5f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+        }
+        AnimationHandler();
+    }
+
+    private  void AnimationHandler()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("isRunning", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("isRunning", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
         }
     }
 }
