6e1cdb894a0fa17599d4f1c6827c17dc4d171a91
halilkeklik <halil@halilkeklik.com>
2023-02-04 23:00:09 +0300

    change player combat

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -4,34 +4,20 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    [SerializeField] private Animator anim;
     [SerializeField] private float damage;
     [SerializeField] private float attackRangeX;
     [SerializeField] private float attackRangeY;
     [SerializeField] private Transform attackPos;
     [SerializeField] private LayerMask whatIsEnemy;
-    private float nextAttackTime = 0f;
-    [SerializeField] private float attackRate;
-    
-    // Update is called once per frame
-    void Update()
+    public bool isAttacking = false;
+    public void Attack()
     {
-        if (Time.time >= nextAttackTime)
-        {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                Attack();
-                nextAttackTime = Time.time + 1f / attackRate;
-            }
-        }
-    }
-    
-    // ReSharper disable Unity.PerformanceAnalysis
-    void Attack()
-    {
-        anim.SetTrigger("attack");
         Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll(attackPos.position,
             new Vector2(attackRangeX, attackRangeY),0,whatIsEnemy);
+        
+        if(enemiesToDamage==null)
+            return;
+        
         foreach (var t in enemiesToDamage)
         {
             t.GetComponent<Health>().TakeDamage(damage);
